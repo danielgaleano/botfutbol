25a249a39a277647882f75dd19592decaf21cb14
danielgaleano <ramgalb@gmail.com>
2021-02-22 16:12:54 -0300

    Added emojis to the tweet. Added docstrings to the functions

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
-from datetime import datetime, timedelta, timezone
+""" An example of a Twitter Bot
+
+It gets predictions from Football Prediction API and tweets
+the prediction about a random soccer match on a Twitter account.
+
+"""
+
+from datetime import datetime, timezone
 import time
 import os
-import requests
-import pprint
 import random
+import traceback
+import requests
 import pytz
 import tweepy
-import traceback
+import emoji
 
 INTERVAL = 60 * 60 * 1  # tweet every 1 hour
 
@@ -17,16 +24,35 @@ local_tz = pytz.timezone("America/Asuncion")
 
 
 def get_current_datetime_on_api_server():
-    london_time = datetime.now(tz=timezone.utc).astimezone(api_tz)
-    return london_time
+    """Get the current datetime on the api server.
+
+    :return: datetime on the api server.
+    :rtype: datetime.datetime
+    """
+
+    api_time = datetime.now(tz=timezone.utc).astimezone(api_tz)
+    return api_time
 
 
 def to_local_datetime(start_date):
-    dt = datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S")
-    return api_tz.localize(dt).astimezone(local_tz)
+    """Get the current local datetime.
+
+    :param start_date: a datetime to convert to local timezone.
+    :return: Local datetime
+    :rtype: datetime.datetime
+    """
+
+    date_t = datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S")
+    return api_tz.localize(date_t).astimezone(local_tz)
 
 
 def get_prediction():
+    """Get a random prediction from the API.
+
+    :return: A dictionary with the prediction data
+    :rtype: dict
+    """
+
     prediction_data = None
 
     # setting our API key for auth
@@ -41,30 +67,41 @@ def get_prediction():
         "market": "classic"
     }
 
-    url = "https://football-prediction-api.p.rapidapi.com/api/v2/predictions"
-    response = requests.request("GET", url, headers=headers, params=params)
+    try:
+        url = "https://football-prediction-api.p.rapidapi.com/api/v2/predictions"
+        response = requests.request("GET", url, headers=headers, params=params)
 
-    if response.ok:
-        data = response.json().get('data')
+        if response.ok:
+            data = response.json().get('data')
 
-        # Filter expired events
-        active_events = [event for event in data if not event.get('is_expired')]
+            # Filter expired events
+            active_events = [event for event in data if not event.get('is_expired')]
 
-        # Select event
-        selected = random.choice(active_events)
+            # Select event
+            selected = random.choice(active_events)
 
-        # Get event details
-        prediction_id = selected.get('id')
-        detail_url = f"https://football-prediction-api.p.rapidapi.com/api/v2/predictions/{prediction_id}"
-        response_detail = requests.request("GET", detail_url, headers=headers)
+            # Get event details
+            p_id = selected.get('id')
+            d_url = f"https://football-prediction-api.p.rapidapi.com/api/v2/predictions/{p_id}"
+            response_detail = requests.request("GET", d_url, headers=headers)
 
-        if response_detail.ok:
-            prediction_data = response_detail.json().get('data').pop()
+            if response_detail.ok:
+                prediction_data = response_detail.json().get('data').pop()
+
+    except requests.exceptions.RequestException as request_error:
+        traceback.print_exc()
+        print(request_error)
 
     return prediction_data
 
 
 def configure_twitter_access():
+    """Configure Twitter access and authentication
+
+    :return: Twitter API wrapper
+    :rtype: tweepy.api
+    """
+
     consumer_key = os.getenv("TW_CONSUMER_KEY")
     consumer_secret = os.getenv("TW_CONSUMER_SECRET")
     access_token = os.getenv("TW_ACCESS_TOKEN")
@@ -72,17 +109,25 @@ def configure_twitter_access():
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    
-    return api
+    tw_api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    return tw_api
+
 
 def format_tweet(data):
+    """Format the text to publish on Twitter.
+
+    :param data: A dictionary with the prediction data.
+    :return: Formated text to tweet
+    :rtype: string
+    """
+
     competition = data.get('competition_cluster')
     competition_name = data.get('competition_name')
     home_team = data.get('home_team')
     away_team = data.get('away_team')
     draw = 'Empate'
-    
+
     prediction = data.get('prediction_per_market').get('classic').get('prediction')
 
     possibilities = {
@@ -94,34 +139,42 @@ def format_tweet(data):
         '12': f'{home_team} o {away_team}'
     }
 
+    p_emoji = ':point_right:'
+    if len(prediction) > 1:
+        p_emoji = ':person_shrugging:'
+
+
     ptext = possibilities.get(prediction)
 
     tweet = f"""
         {competition} {competition_name}: 
-        {home_team} - {away_team}
-        Pronóstico: {ptext}
+        :stadium: {home_team} - {away_team}
+        {p_emoji} Pronóstico: {ptext}
 
     """
 
-    return tweet
+    return emoji.emojize(tweet, use_aliases=True)
 
 
-def tweet(api):
+def publish_tweet(tw_api):
+    """Publish text on Twitter Bot Account
+
+    :param tw_api: the Twitter API wrapper to publish the tweet.
+    """
+
     raw_prediction = get_prediction()
 
     if raw_prediction:
         text = format_tweet(raw_prediction)
-        api.update_status(text)
+        try:
+            tw_api.update_status(text)
+        except tweepy.TweepError as tweepy_error:
+            traceback.print_exc()
+            print(tweepy_error)
 
 
 if __name__ == "__main__":
     api = configure_twitter_access()
     while True:
-        try:
-            tweet(api)
-        except Exception as e:
-            traceback.print_exc()
-            print(e)
-
+        publish_tweet(api)
         time.sleep(INTERVAL)
-
